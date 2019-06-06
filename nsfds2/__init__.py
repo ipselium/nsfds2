e2cd405d4969a83b5888582b1a6e1bc699839154
ipselium <ipselium@free.fr>
2019-06-06 17:22:57 +0200

    Bump version: 0.9.0 → 0.9.1

--- nsfds2/__init__.py
+++ nsfds2/__init__.py
@@ -22,5 +22,5 @@
 # Creation Date : 2019-03-07 - 23:02:29
 
 
-__version__ = "0.9.0"
+__version__ = "0.9.1"
 __all__ = ['fdtd', 'init', 'utils']
--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     description="Finite difference solver for Navier-Stokes equations",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    version="0.9.0",
+    version="0.9.1",
     license="GPL",
     url='https://github.com/ipselium/nsfds2',
     author="Cyril Desjouy",
