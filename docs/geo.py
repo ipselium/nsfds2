b706840266fbf0a126a68815ad9fd4f899ac5904
ipselium <ipselium@free.fr>
2019-06-21 15:30:05 +0200

    chg: update docs

--- docs/geo.py
+++ docs/geo.py
@@ -41,6 +41,16 @@ def logo(xn, zn):
         + _np.linspace(0.5, 0, zn.shape[1])*(100*xp**2)
     return xp, zp
 
+def circle(xn, zn):
+    """ circle """
+
+    width = xn[-1, 0] - xn[0, 0]
+    R = width/(2*_np.pi)
+    xp = (zn + R)*_np.sin(xn/R)
+    zp = (zn + R)*_np.cos(xn/R)
+
+    return xp, zp
+
 
 def letter_a(nx, nz, size=10):
     """ letter a.
@@ -76,11 +86,55 @@ def moving_square(nx, nz, size_percent=20):
 
     size = int(min(nx, nz)*size_percent/100)
     obs1 = Obstacle([int(nx/2)-size, int(nz/2)-size,
-                     int(nx/2)+size, int(nz/2)+size], 'URRR')
-    obs2 = Obstacle([nx-11, 0, nx-1, nz-1], 'URRR')
+                     int(nx/2)+size, int(nz/2)+size], 'VRRR')
+    obs2 = Obstacle([nx-11, 0, nx-1, nz-1], 'VRRR')
 
     obs1.set_moving_bc({'f': 70000, 'A': 1, 'func': 'sine'})
     obs2.set_moving_bc({'f': 73000, 'A': 1, 'func': 'sine'})
+
+    return Domain((nx, nz), data=[obs1, obs2])
+
+
+def ceperley(nx, nz, source_size=5):
+    """ Ceperley resonator.
+    """
+
+    source_size = int(nx*source_size/100)
+    loc1 = int(nx/4)
+    loc2 = int(nx/2)
+
+    obs1 = Obstacle([0, 0, nx-1, 11], 'RRRR')
+    obs2 = Obstacle([0, nz-11, loc1 , nz-1], 'RRRR')
+    obs3 = Obstacle([loc1, nz-11, loc1+source_size, nz-1], 'RVRR')
+    obs4 = Obstacle([loc1+source_size, nz-11, loc2, nz-1], 'RRRR')
+    obs5 = Obstacle([loc2, nz-11, loc2+source_size, nz-1], 'RVRR')
+    obs6 = Obstacle([loc2+source_size, nz-11, nx-1, nz-1], 'RRRR')
+
+    obs3.set_moving_bc({'f': 4/3, 'A': 100, 'func': 'sine', 'kwargs':{'n':2}})
+    obs5.set_moving_bc({'f': 4/3, 'A': -100, 'func': 'sine', 'kwargs':{'n':2}})
+
+    return Domain((nx, nz), data=[obs1, obs2, obs3, obs4, obs5, obs6])
+
+
+def wav_squares(nx, nz, size_percent=5):
+    """ Square in the middle.
+
+    Parameters:
+    -----------
+
+    size_percent (float): size of the square in percent of the largest
+    dimension of the domain.
+    """
+
+    size = int(min(nx, nz)*size_percent/100)
+
+    obs1 = Obstacle([int(nx/2)-size, int(nz/4),
+                     int(nx/2)+size, int(nz/4)+2*size], 'RRVR')
+    obs2 = Obstacle([int(nx/2)-size, int(3*nz/4)-2*size,
+                     int(nx/2)+size, int(3*nz/4)], 'RRVR')
+
+    obs1.set_moving_bc({'f': '~/.nsfds2/spam3.wav', 'A': 0.05, 'func': 'tukey'})
+    obs2.set_moving_bc({'f': '~/.nsfds2/spam3.wav', 'A': -0.05, 'func': 'tukey'})
 
     return Domain((nx, nz), data=[obs1, obs2])
 
