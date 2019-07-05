62a4e70341fb951acd348bb368ef447a5c0ac39d
ipselium <ipselium@free.fr>
2019-07-06 01:12:45 +0200

    Bump version: 0.9.11 → 0.9.12

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ author = 'Cyril Desjouy'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.9.11'
+release = '0.9.12'
 
 
 # -- General configuration ---------------------------------------------------
--- nsfds2/__init__.py
+++ nsfds2/__init__.py
@@ -31,5 +31,5 @@ if sys.platform == 'darwin':
     import matplotlib
     matplotlib.use("TkAgg")
 
-__version__ = "0.9.11"
+__version__ = "0.9.12"
 __all__ = ['fdtd', 'init', 'utils']
