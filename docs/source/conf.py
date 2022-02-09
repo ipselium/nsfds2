f88277cb364f2958f0767d5823c6b277b62145ed
ipselium <ipselium@free.fr>
2022-02-09 15:35:56 +0100

    Bump version: 0.11.5 → 0.11.6

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ author = 'Cyril Desjouy'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.11.5'
+release = '0.11.6'
 
 
 # -- General configuration ---------------------------------------------------
